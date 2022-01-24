1f5347e190aa6dec38fa51e259418cea8fdfab8d
olgaml <90445794+olgaml@users.noreply.github.com>
2022-01-24 21:27:36 +0200

    Update nodejs_docker.groovy

--- basics/dsl/nodejs_docker.groovy
+++ basics/dsl/nodejs_docker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/olgaml/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@devophift.work')
         }
@@ -12,7 +12,7 @@ job('NodeJS Docker example') {
     
     steps {
         dockerBuildAndPublish {
-            repositoryName('olgaml/development')
+            repositoryName('olgaml/development|dslExam')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('adbde24d-2b26-4be6-ae78-072aeb5d3514')
             buildContext('./basics/')
